c37c438e56522839eef2f55a83b0a3f8e379435d
GowthamNats <72858459+GowthamNats@users.noreply.github.com>
2023-04-24 09:03:05 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -20,7 +20,7 @@ function Card({tags, title, ttr, button, image, desc, link}) {
       {openModal && 
         <>
           <div className="flex justify-center items-center fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-50">
-            <div className="relative w-full m-10">
+            <div className="relative w-full m-2 md:m-10">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
                   <h3 className="text-3xl font=semibold">{title}</h3>
